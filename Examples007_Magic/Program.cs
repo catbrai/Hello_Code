28b7dd662da61d8fe226332b1d1871236b5bb491
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-21 00:17:02 +0300

    Examples007_Magic

---
+++ Examples007_Magic/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 10, ya = 1;
+int xb = 1, yb = 10;
+int xc = 20, yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
