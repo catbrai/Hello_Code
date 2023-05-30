c9eb8aaad9071a633c59acfe456b7fe38f9b319f
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-30 09:24:28 +0300

    Task13,14,15

---
+++ Examples13_For/Program.cs
@@ -0,0 +1,13 @@
+﻿string Metod4(int count, string text)
+{
+string result = String.Empty;
+for (int i = 0; i<count; i++) // вначале ключевое слово, затем инициализация
+//счётчика, после проверка условия и инкремент (увеличение счётчика).
+{
+result = result + text;
+}
+return result;
+}
+string res = Metod4(10, "aaa");
+Console.WriteLine(res);
+
---
+++ Examples14_MultiTable/Program.cs
@@ -0,0 +1,9 @@
+﻿for (int i = 2; i <= 10; i++)
+{
+for (int j = 2; j <= 10; j++)
+{
+Console.WriteLine($"{i, 2} * {j, 2} = {i * j, 3}");
+}
+Console.WriteLine();
+}
+
